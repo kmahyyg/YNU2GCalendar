8b77e1e679f56fd9e09616d22480a1c8b0d99bce
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2018-03-24 19:47:24 +0800

    test(client.py): test for exporting to a ics file

--- client.py
+++ client.py
@@ -4,6 +4,9 @@
 # The client side is responsible for their class information and oauth authorization code
 
 from fuckehell import *
+from helljson_proc import *
+from icsgen import *
+from sentry import sendlog_sent
 
 
 def main():
@@ -16,11 +19,22 @@ def main():
     print("PLEASE READ README.md first!!!!!!")
     input("\n Press any key to continue")
     ehall_ckie = getcookie()
+    ynucal = init_cal()
     print("Ehall cookies emulate successfully finished!")
     totalweek = int(input("Please input the total weeks in this semester: ___"))
+
     for i in range(1, totalweek + 1):
         cls = getclassjson(ehall_ckie, i)
-
+        print('Classes in Week ' + str(i) + ' is gotten.')
+        try:
+            for j in cls:
+                evntresc = generate_event(j, i)
+                evntical = crea_evnt(evntresc)
+                ynucal = add_evnt(ynucal, evntical)
+        except:
+            sendlog_sent()
+    export2f(ynucal)
+    print('Export successfully finished.')
     return 0
 
 
