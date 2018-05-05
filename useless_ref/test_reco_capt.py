90787595c83331c9e2bc3985de75cdf5dc2ce8af
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2018-05-05 14:57:43 +0800

    test(test_reco_capt.py): test performance after manually correct
    RESULT: 65%
    
    BREAKING CHANGE: Introduce OCR the captcha image

--- useless_ref/test_reco_capt.py
+++ useless_ref/test_reco_capt.py
@@ -4,21 +4,57 @@
 from PIL import Image
 import pytesseract
 import requests
+import os
 
-captimage = requests.get('http://ids.ynu.edu.cn/authserver/captcha.html', stream=True, allow_redirects=True)
-capimgsaved = open('testcap.jpg', 'wb')
-capimgsaved.write(captimage.content)
-capimgsaved.close()
-imgcapt = Image.open('testcap.jpg')
-try:
-    global optcode
-    optcode = pytesseract.image_to_string(imgcapt)
-except:
-    pass
-if len(optcode) == 0:
-    imgcapt = imgcapt.convert('L')
-    # TODO: image pre-process
-if len(optcode) >= 4:
-    optcode = optcode[:4]
-print('Captcha is:', optcode)
-# ask user to do a confirmation
+correctence = []
+
+def imgpreproc(imgobj):
+    imgobj = imgobj.convert('L')
+    threshold = 125
+    table = []
+    for i in range(256):
+        if i < threshold:
+            table.append(0)
+        else:
+            table.append(1)
+    imgobj = imgobj.point(table,'1')
+    imgobj.show()
+    opt_seccode = pytesseract.image_to_string(imgobj)
+    return opt_seccode
+
+
+def confirm_correct():
+    choice1 = input("Correct?(Y/N)")
+    if choice1 == 'Y':
+        correctence.append(1)
+        return True
+    else:
+        correctence.append(0)
+        correctname = input("Correct one is?")
+        return correctname
+
+
+def main():
+    captimage = requests.get('http://ids.ynu.edu.cn/authserver/captcha.html', stream=True, allow_redirects=True)
+    capimgsaved = open('testcap.jpg', 'wb')
+    capimgsaved.write(captimage.content)
+    capimgsaved.close()
+    imgcapt = Image.open('testcap.jpg')
+    optseccode = str(imgpreproc(imgcapt))
+    optseccode = optseccode.replace(' ','')
+    print('After Process, Possible: '+ optseccode)
+    corre = confirm_correct()
+    if isinstance(corre,bool):
+        newname = str(optseccode) + '.jpg'
+        os.rename('testcap.jpg',newname)
+    elif isinstance(corre, str):
+        newname = corre + '.jpg'
+        os.rename('testcap.jpg',newname)
+
+if __name__ == '__main__':
+    for i in range(100):
+        main()
+    print('Correct:' + str(correctence.count(1)) + '/100')
+
+
+    # TEST RESULT:  65/100 CORRECT! X and >< cannot be correctly recogized!
