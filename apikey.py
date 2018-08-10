cc005ae792389652fbda5555fdb4acac8462c1db
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2018-08-10 16:00:14 +0800

    fix(apikey&requirements): add lxml dependencies, add interactive user-pass auth

---
+++ apikey.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+from datetime import datetime
+
+from pytz import timezone
+
+# hardcoded
+bjtz = timezone('Asia/Shanghai')
+sentryid = 'https://43b3b4ea4f724fda919fdfc7af178a25:d3ba0a16090041b592701abb106da138@sentry.io/297885'  # Don't modify
+
+startday = input("When will the semester Begin? yyyy.mm.dd")
+sembegin = startday.split(".")
+
+semester_start = datetime(int(sembegin[0]), int(sembegin[1]), int(sembegin[2]), 0, 0, 0, tzinfo=bjtz)  # Semester starts at
+
+# read from varenv whem compiled into an exe
+ynu_ehell_name = input('Ehall Username:')  # Your EHALL SYSTEM Username
+ynu_ehell_password = input('Password:')  # Your EHALL SYSTEM Password
+mailacc = 'MAILTO:'+ input('Your email:')  # Your email account
