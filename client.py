207c32cb3c0eaed99ebbb6b0d51e5e8665db9a47
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2019-02-23 19:37:21 +0800

    fix some issue

--- client.py
+++ client.py
@@ -22,18 +22,19 @@ def main():
     ehall_ckie = getcookie()
     ynucal = init_cal()
     print("Ehall cookies emulate successfully finished!")
-    totalweek = input("Please input the total weeks in this semester: ___")
+    totalweek = input("Please input the total weeks in this semester: ")
     totalweek = int(totalweek)
     for i in range(1, totalweek + 1):
         cls = getclassjson(ehall_ckie, i, current_term)
         allcls = cls['rows']
-        print('Classes in Week ' + str(i) + ' is gotten.')
+        print('Classes in Week ' + str(i) + ' are gotten.')
         try:
             for j in allcls:
                 evntresc = generate_event(j, i)
                 evntical = crea_evnt(evntresc)
                 ynucal = add_evnt(ynucal, evntical)
         except:
+            print("You may encounter a problem due to unstable network connection in YNU, try again later.")
             sendlog_sent()
     export2f(ynucal)
     print('Export successfully finished.')
@@ -42,9 +43,8 @@ def main():
 
 if __name__ == '__main__':
     print('Please read this disclaimer first: https://github.com/kmahyyg/YNU2GCalendar#disclaimer')
-    print('Do you accept it? (Y/N)')
-    choice = input()
-    if choice == 'Y':
+    choice = input("Do you accept it? (Y/N)")
+    if choice.upper() == 'Y':
         main()
     else:
         print('See you!')
